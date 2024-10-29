286a747e4fbd276426361db6a4663c8a13f73a67
careybarberjr@gmail.com <careybarberjr@gmail.com>
2024-10-29 14:43:17 -0400

    e

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,19 +2,39 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int month, year, day;
+
+	cout << "enter a month(1 - 12):" << endl;
+	cin >> month;
+
+	cout << "enter year:" << endl;
+	cin >> year;
+
+	if (year % 400 == 0 || year % 4 == 0)
+	{
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+			day == 31;
+		else if (month == 4 || month == 6 || month == 9 || month == 11)
+			day == 30;
+		else
+			day == 29;
+	}
+	else if (year % 400 != 0 || year % 4 != 0)
+	{
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+			day == 31;
+		else if (month == 4 || month == 6 || month == 9 || month == 11)
+			day == 30;
+		else
+			day == 28;
+	}
+
+	cout << "days" << day << endl;
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
