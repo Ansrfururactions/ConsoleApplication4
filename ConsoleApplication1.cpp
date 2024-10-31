b2758fca47b3767a94ca3d9cdd727b92dafad307
careybarberjr@gmail.com <careybarberjr@gmail.com>
2024-10-30 21:05:04 -0400

    a

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,20 +1,56 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cmath>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int options=0;//defines & initiallizes the variable used for selecting options
+	double area=0.0, base = 0.0, radius, height = 0.0, length = 0.0, width = 0.0;// defines & initiallizes variables used in the equations
+
+	while (options <= 0 || options > 4) // provides a failsafe for when the user enters an invalid option
+	{
+		cout << "type a number from 1-4 to select the following options: " << endl << "1. Calculate the Area of a Circle" << endl << "2. Calculate the Area of a Rectangle" << endl << "3. Calculate the Area of a Triangle" << endl << "4. Quit" << endl;//provides the options for the user to enter
+		cin >> options;//stores the option as an integer
+
+		if (options <= 0 || options > 4)//acts as a trigger for the following cout statement
+			cout << "this is not an option provided, please try again." << endl;//lets user know that they entered an ivalid option
+	}
+
+	if (options == 1)//runs option 1 if selected
+	{
+		cout << "enter radius" << endl;//asks for radius
+		cin >> radius;//stores radius
+
+		area = 3.14159 * pow(radius, 2);// calculates area
+
+		cout << "the area of your circle is " << area << endl;//displays area
+	}
+	else if (options == 2)
+	{
+		cout << "enter length" << endl;//asks for length
+    	cin >> length;//stores length
+
+		cout << "enter width" << endl;//asks for width
+		cin >> width;//stores width
+
+		area = length * width;// calculates area
+
+		cout << "the area of your rectangle is " << area << endl;//displays area
+	}
+	else if (options == 3)
+	{
+		cout << "enter base" << endl;//asks for base
+		cin >> base;//stores base
+
+		cout << "enter height" << endl;//asks for height
+		cin >> height;//stores height
+
+		area = base * height * 0.5;// calculates area
+
+		cout << "the area of your triangle is " << area << endl; //displays area
+	}
+	else
+	{
+		return 0; //terminates the program
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
